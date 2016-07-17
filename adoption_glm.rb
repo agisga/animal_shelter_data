cc062070d167d2e98121207a9ef4a473e027da31
agisga <alexej.go@googlemail.com>
2016-07-16 22:43:15 -0700

    glm that predicts the probability of adoption on test data

---
+++ adoption_glm.rb
@@ -0,0 +1,18 @@
+require 'daru'
+require 'statsample-glm'
+
+train_data = Daru::DataFrame.from_csv 'animal_shelter_train_processed.csv'
+
+# define the 0-1-valued response variable
+train_data['Adoption'] = (train_data['OutcomeType'].contrast_code)['OutcomeType_Adoption']
+
+# fit the model
+formula = 'Adoption~AnimalType+Breed+AgeuponOutcome+Color+SexuponOutcome'
+glm_adoption = Statsample::GLM::Regression.new formula, train_data, :logistic
+
+# predict on test data
+test_data = Daru::DataFrame.from_csv 'animal_shelter_train_processed.csv'
+adoption_pred = glm_adoption.predict test_data 
+
+# save the result
+File.open('adoption_pred.txt', 'w') { |f| f.puts adoption_pred.to_a }

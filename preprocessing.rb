826d9a5e5659f76efdcd7d998d7cd42dfe75998b
agisga <alexej.go@googlemail.com>
2016-07-16 22:01:35 -0700

    preprocessing the animal shelter training data for logistic regression

---
+++ preprocessing.rb
@@ -0,0 +1,65 @@
+require 'daru'
+
+shelter_data = Daru::DataFrame.from_csv 'animal_shelter_train.csv'
+
+# get rid of some of the variables
+shelter_data.delete_vectors *%W[AnimalID Name DateTime OutcomeSubtype]
+
+# get rid of rows with missing values
+shelter_data = shelter_data.filter_rows do
+  |row| !(row.has_missing_data? or row['SexuponOutcome'] == "Unknown")
+end
+
+puts "Are there any missing values left?"
+puts shelter_data.has_missing_data?
+shelter_data.vectors.to_a.each do |i| 
+  puts shelter_data[i].include? ""
+  puts shelter_data[i].include? "Unknown"
+end
+
+# transform AgeuponOutcome to a numeric variable
+shelter_data['AgeuponOutcome'].map! do |age|
+  num, unit = age.split
+  num = num.to_f
+  case unit
+  when "year", "years"
+    52.0 * num
+  when "month", "months"
+    4.5 * num
+  when "week", "weeks"
+    num
+  when "day", "days"
+    num / 7.0
+  else
+    raise "Unknown AgeuponOutcome unit!"
+  end  
+end
+
+# define categorical variables
+shelter_data.to_category 'OutcomeType', 'AnimalType', 'SexuponOutcome', 'Breed', 'Color'
+
+# recode Breed
+other_breed = shelter_data['Breed'].categories.select { |i| shelter_data['Breed'].count(i) < 100 }
+other_breed_hash = other_breed.zip(['other']*other_breed.size).to_h
+shelter_data['Breed'].rename_categories other_breed_hash
+shelter_data['Breed'].base_category = 'other'
+shelter_data['Breed'].frequencies
+
+# recode Color
+other_color = shelter_data['Color'].categories.select { |i| shelter_data['Color'].count(i) < 100 }
+other_color_hash = other_color.zip(['other']*other_color.size).to_h
+shelter_data['Color'].rename_categories other_color_hash
+shelter_data['Color'].base_category = 'other'
+shelter_data['Color'].frequencies
+
+# save result
+shelter_data.write_csv "animal_shelter_train_processed.csv"
+
+## define the 0-1-valued response variable
+#shelter_data['OutcomeType_Adoption'] = (shelter_data['OutcomeType'].
+#                                        contrast_code)['OutcomeType_Adoption']
+#
+#require 'statsample-glm'
+#formula = 'OutcomeType_Adoption~AnimalType+Breed+AgeuponOutcome+Color+SexuponOutcome'
+#glm_adoption = Statsample::GLM::Regression.new formula, small, :logistic
+#puts glm_adoption.model.coefficients :hash

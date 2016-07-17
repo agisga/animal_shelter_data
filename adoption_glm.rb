39a0340fc5880d7f9f1345f40137cb5879e92bd4
agisga <alexej.go@googlemail.com>
2016-07-16 22:53:26 -0700

    small correction

--- adoption_glm.rb
+++ adoption_glm.rb
@@ -4,6 +4,7 @@ require 'statsample-glm'
 train_data = Daru::DataFrame.from_csv 'animal_shelter_train_processed.csv'
 
 # define the 0-1-valued response variable
+train_data.to_category 'OutcomeType'
 train_data['Adoption'] = (train_data['OutcomeType'].contrast_code)['OutcomeType_Adoption']
 
 # fit the model

be68de33d6c3ff80bec15b0f9e2182a80cfb6ef9
agisga <alexej.go@googlemail.com>
2016-07-17 20:07:30 -0700

    combined GLM results in the form as required by kaggle

--- combine_results.rb
+++ combine_results.rb
@@ -7,12 +7,16 @@ euthanasia_pred = IO.readlines("euthanasia_pred.txt").map { |i| i.to_f }
 transfer_pred = IO.readlines("transfer_pred.txt").map { |i| i.to_f }
 return_to_owner_pred = IO.readlines("return_to_owner_pred.txt").map { |i| i.to_f }
 
+require 'daru'
 shelter_data = Daru::DataFrame.from_csv 'animal_shelter_test.csv'
-glm_results = Daru::DataFrame.new({AnimalID: shelter_data['ID'].to_a,
+glm_results = Daru::DataFrame.new({ID: shelter_data['ID'].to_a,
                                    Adoption: adoption_pred,
                                    Died: died_pred,
                                    Euthanasia: euthanasia_pred,
                                    Return_to_owner: return_to_owner_pred,
-                                   Transfer: transfer_pred})
+                                   Transfer: transfer_pred},
+                                 order: [:ID, :Adoption, :Died,
+                                         :Euthanasia, :Return_to_owner,
+                                         :Transfer])
 
 glm_results.write_csv "combined_pred.csv"
